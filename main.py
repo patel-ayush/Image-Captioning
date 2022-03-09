7341533426b612efbd7a5225ce52180736bc9949
Ayush Patel <ayushpatelnakish@gmail.com>
2022-03-09 19:09:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if uploaded_file is not None:
         pass
     with col3:
         st.markdown(f"<center><b style='text-align: center;font-family:Brush Script MT, cursive;font-size: 25px;'>Here in your caption :</b></center>", unsafe_allow_html=True)
-        gif_runner = st.image("ic_load_!.gif")
+        gif_runner = st.image("ic_load_1.gif")
         st.markdown(f"<center><b style='text-align: center;font-family:Brush Script MT, cursive;font-size: 25px;color:MediumSeaGreen'>\"{predict(opencv_image)}\" </b><br></center>", unsafe_allow_html=True)
         gif_runner.empty()
 
