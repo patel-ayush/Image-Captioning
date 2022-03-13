1a3d51267fc3d51c7465499a8fe0c95c36504ec6
Ayush Patel <ayushpatelnakish@gmail.com>
2022-03-13 23:05:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,10 @@ if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     opencv_image=cv2.cvtColor(opencv_image,cv2.COLOR_BGR2RGB)
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     with col1:
         st.image(opencv_image, caption='Uploaded Image.', use_column_width=True,clamp=True,)
     with col2:
-        pass
-    with col3:
         st.markdown(f"<center><b style='text-align: center;font-family:Brush Script MT, cursive;font-size: 25px;'>Here in your caption :</b></center>", unsafe_allow_html=True)
         gif_runner = st.image("ic_load_1.gif")
         st.markdown(f"<center><b style='text-align: center;font-family:Brush Script MT, cursive;font-size: 25px;color:MediumSeaGreen'>\"{predict(opencv_image)}\" </b><br></center>", unsafe_allow_html=True)
